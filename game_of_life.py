3a3d26f3568538cd8087ee00c84fac1192e14878
Alexandr Kuzmin <shurik.kuzmin@gmail.com>
2022-02-19 10:32:06 -0500

    Adding drawing of the field.

--- game_of_life.py
+++ game_of_life.py
@@ -10,21 +10,34 @@ import numpy
 import pygame
 
 size = 100
-field = numpy.zeros((size, size))
+field = numpy.random.randint(0, 2, (size, size))
 
 pygame.init()
 
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
 WINDOW = pygame.display.set_mode((500,500))
-WINDOW.fill((0,0,0))
+WINDOW.fill(BLACK)
+
+def draw_field(field):
+    for i in range(size):
+        for j in range(size):
+            if field[i][j] == 1:
+                pygame.draw.rect(WINDOW, WHITE, (5*j, 5*i, 5, 5))
+
 
 isActive = True
 while isActive:
+    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             isActive = False
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 isActive = False
+    draw_field(field)
+    pygame.display.update()
 
 pygame.quit()
 
